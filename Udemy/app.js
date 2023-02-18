8f21743d639e534fcccddb3e4ef7fc61a6f56e4b
조민수 <choalstn@gmail.com>
2023-02-18 18:20:25 +0900

    데이터 타입 - 튜플

--- Udemy/app.js
+++ Udemy/app.js
@@ -1,1 +1,29 @@
-console.log("yout code is here");
+// 포괄적인 타입 지정
+// const person : object = {
+//     name : "minsu",
+//     age : 24,
+// };
+// const person : {
+//     name : string,
+//     age : number
+// } = {
+//     name : "minsu",
+//     age : 24
+// }
+var person = {
+    name: 'minsu',
+    age: 24,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
+};
+// Push가 되긴 함
+// person.role.push('admin');
+//두 번째 인자는 string이기 때문에 불가능
+// person.role[1] = 10;
+var favoriteActivites;
+favoriteActivites = ["Sports", "Cooking"];
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
+console.log(person.name);
--- Udemy/app.ts
+++ Udemy/app.ts
@@ -1,1 +1,40 @@
-console.log("yout code is here")
+// 포괄적인 타입 지정
+// const person : object = {
+//     name : "minsu",
+//     age : 24,
+// };
+
+// const person : {
+//     name : string,
+//     age : number
+// } = {
+//     name : "minsu",
+//     age : 24
+// }
+
+const person: {
+    name : string,
+    age :  number,
+    hobbies : string[],
+    role : [number, string]
+} = {
+    name : 'minsu',
+    age : 24,
+    hobbies : ['Sports', 'Cooking'],
+    role: [2, 'author']
+}
+
+// Push가 되긴 함
+// person.role.push('admin');
+
+//두 번째 인자는 string이기 때문에 불가능
+// person.role[1] = 10;
+
+let favoriteActivites : string[] ; 
+favoriteActivites = ["Sports", "Cooking"];
+
+for(const hobby of person.hobbies) {
+    console.log(hobby)
+}
+
+console.log(person.name)
