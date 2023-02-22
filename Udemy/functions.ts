a4092d210f4539891ffcf79527bec82723c9ea8b
조민수 <choalstn@gmail.com>
2023-02-22 20:00:39 +0900

    기존 app.ts 파일 이름 변경

---
+++ Udemy/functions.ts
@@ -0,0 +1,29 @@
+function addUdemy(n1 : number, n2:number) {
+    return n1 + n2
+}
+
+// void 타입 반환 
+function printResult(num : number) {
+    console.log('Result : ' + num)
+}
+
+function addAndHandle(n1:number, n2:number, callback: (num : number) => void) {
+    const result = n1 + n2;
+    callback(result);
+
+}
+
+printResult(addUdemy(5,12))
+
+let combinedValues : (a:number, b:number) => number;
+
+combinedValues = addUdemy;
+
+// printResult는 void 타입이며 인자를 한 개만 받고 있기 때문에 불가능 
+// combinedValues = printResult;
+
+console.log(combinedValues(8,8))
+
+addAndHandle(10, 20, (result) => {
+    console.log(result)
+});
