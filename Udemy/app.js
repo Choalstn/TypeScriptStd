5f2bf9a82bd115c1fecfeb387f7b0098207f91ca
조민수 <choalstn@gmail.com>
2023-02-22 20:17:13 +0900

    never 타입

--- Udemy/app.js
+++ Udemy/app.js
@@ -10,3 +10,7 @@ console.log(userName);
 // any와 비슷한 역할이지만 아래 코드에서 차이점이 나타남
 // userInput이 unknown이면 에러, any면 에러x
 // userName = userInput;
+function generateErrors(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateErrors('An Error Occurred !', 500);
