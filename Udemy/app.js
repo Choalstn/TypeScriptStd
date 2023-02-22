b91b30c4513af593337cedc48f31094f40fc0c0f
조민수 <choalstn@gmail.com>
2023-02-22 19:45:27 +0900

    함수 타입 (2) & 콜백

--- Udemy/app.js
+++ Udemy/app.js
@@ -5,9 +5,16 @@ function addUdemy(n1, n2) {
 function printResult(num) {
     console.log('Result : ' + num);
 }
+function addAndHandle(n1, n2, callback) {
+    var result = n1 + n2;
+    callback(result);
+}
 printResult(addUdemy(5, 12));
 var combinedValues;
 combinedValues = addUdemy;
 // printResult는 void 타입이며 인자를 한 개만 받고 있기 때문에 불가능 
 // combinedValues = printResult;
 console.log(combinedValues(8, 8));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
--- Udemy/app.ts
+++ Udemy/app.ts
@@ -7,6 +7,12 @@ function printResult(num : number) {
     console.log('Result : ' + num)
 }
 
+function addAndHandle(n1:number, n2:number, callback: (num : number) => void) {
+    const result = n1 + n2;
+    callback(result);
+
+}
+
 printResult(addUdemy(5,12))
 
 let combinedValues : (a:number, b:number) => number;
@@ -17,3 +23,7 @@ combinedValues = addUdemy;
 // combinedValues = printResult;
 
 console.log(combinedValues(8,8))
+
+addAndHandle(10, 20, (result) => {
+    console.log(result)
+});
