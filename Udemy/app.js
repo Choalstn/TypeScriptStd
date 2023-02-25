36d46c357d63262ef9e7eec22d340cc221ae624e
조민수 <choalstn@gmail.com>
2023-02-26 03:56:04 +0900

    tsc -- init & tsc -w

--- Udemy/app.js
+++ Udemy/app.js
@@ -1,16 +1,3 @@
-var userInput;
-var userName;
-userInput = 5;
-userInput = 'Minsu';
-userName = 'Cho';
-if (typeof userInput === 'string') {
-    userName = userInput;
-}
+"use strict";
+userName = "Minsu";
 console.log(userName);
-// any와 비슷한 역할이지만 아래 코드에서 차이점이 나타남
-// userInput이 unknown이면 에러, any면 에러x
-// userName = userInput;
-function generateErrors(message, code) {
-    throw { message: message, errorCode: code };
-}
-generateErrors('An Error Occurred !', 500);
