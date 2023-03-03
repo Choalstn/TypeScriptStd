68f72a6395146532e724630ac64c2f81ad34240e
조민수 <choalstn@gmail.com>
2023-03-03 16:01:39 +0900

    getter & setter

--- src/app.ts
+++ src/app.ts
@@ -30,8 +30,28 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+    private lastReport: string;
+
+    // getter 메서드는 return 문 필수 
+    get mostRecentReport() {
+        if(this.lastReport) {
+          return this.lastReport;  
+        } else {
+            throw new Error("No report found.")
+        }
+        
+    }
+
+    set mostRecentReport(value:string) {
+        if(!value) {
+            throw new Error("Please pass in a valid value!")
+        }
+        this.addReport(value)
+    }
+
     constructor(id:string, private reports:string[]) {
         super(id, "Accounting")
+        this.lastReport = reports[0];
     }
 
     addEmployee(employee: string): void {
@@ -42,6 +62,7 @@ class AccountingDepartment extends Department {
 
     addReport(text:string) {
         this.reports.push(text)
+        this.lastReport = text;
     }
 
     printReports() {
@@ -62,7 +83,11 @@ console.log(it)
 
 const accounting = new AccountingDepartment('d2', []);
 
+accounting.mostRecentReport = "Year End Report"
+
 accounting.addReport('Something went wrong...');
+
+console.log("mostRecentReport :", accounting.mostRecentReport);
 
 accounting.addEmployee("Minsu");
 accounting.addEmployee("TaeYoung");
