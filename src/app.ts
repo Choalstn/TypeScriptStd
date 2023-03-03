21dfe4df5eb858866d7acd65e5fd7c252f2fed33
조민수 <choalstn@gmail.com>
2023-03-03 15:35:31 +0900

    클래스 상속

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,15 @@
 class Department {
     // private id;
     // private name : string;
-    private employees : string[] = []; //private으로 하면 객체 내에서만 접근 가능 
+    protected employees : string[] = []; //private으로 하면 객체 내에서만 접근 가능 protected는 해당 클래스 뿐만 아니라 자식까지도 사용 가능 
 
-    constructor(private readonly id:string, private name: string) {
+    constructor(private readonly id:string, public name: string) {
         // this.id = id
         // this.name = n;
     }
 
     describe(this:Department) {
-        console.log(`De[artment (${this.id}) : ${this.name} ]`)
+        console.log(`Department (${this.id}) : ${this.name}`)
     }
 
     addEmployee(employee:string) {
@@ -23,12 +23,52 @@ class Department {
     }
 }
 
-const accounting = new Department('d1', "Accounting");
+class ITDepartment extends Department {
+    constructor(id:string, public admins:string[]) {
+        super(id, 'IT')
+    }
+}
 
-console.log(accounting)
+class AccountingDepartment extends Department {
+    constructor(id:string, private reports:string[]) {
+        super(id, "Accounting")
+    }
 
+    addEmployee(employee: string): void {
+        if(employee !== 'Minsu') {
+            this.employees.push(employee)
+        }
+    }
 
-accounting.describe();
+    addReport(text:string) {
+        this.reports.push(text)
+    }
+
+    printReports() {
+        console.log(this.reports)
+    }
+}
+
+const it = new ITDepartment('d1', ['Minsu']);
+
+it.addEmployee('Max');
+it.addEmployee("Minsu")
+
+it.describe();
+it.name = "NEW NAME"
+it.printEmployeeInformation();
+
+console.log(it)
+
+const accounting = new AccountingDepartment('d2', []);
+
+accounting.addReport('Something went wrong...');
+
+accounting.addEmployee("Minsu");
+accounting.addEmployee("TaeYoung");
+
+accounting.printReports();
+accounting.printEmployeeInformation();
 
 // const accountingCopy = {describe : accounting.describe, name : "ms"}
 
