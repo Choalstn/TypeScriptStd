efc77ec098aeaea01234bab6a5ab4cb121c09bb9
조민수 <choalstn@gmail.com>
2023-03-14 17:11:31 +0900

    type guard(typeof, in, instanceof)

--- src/app.ts
+++ src/app.ts
@@ -41,4 +41,63 @@ const el1: ElevatedEmployee = {
 type Combinable = number | string;
 type Numeric = number | boolean;
 
+// Universal의 타입은 number => 유니언 타입의 공통점으로 추측
 type Unversal = Combinable & Numeric;
+
+function add(a:Combinable, b:Combinable) {
+    // 아래 조건문이 바로 타입가드 -> union 타입이 지닌 유연성을 활용할 수 있게 해주며 런타임 시 코드가 정확하게 작동하게 도와주기 떄문
+    if(typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+
+    return a+b;
+}
+
+type UnkownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp:UnkownEmployee) {
+    // 두 번째 형태의 타입가드 = in 키워드 사용 
+    if('previleges' in emp) {
+        console.log('Previleges : ', emp.previleges)
+    }
+
+    if('startDate' in emp) {
+        console.log('StartDate : ', emp.startDate)
+    }
+
+    // 두 타입 모두 갖고 있는 속성이기에 오류가 나지 않는다 
+    console.log('Name : ', emp.name);
+}
+
+printEmployeeInformation(el1)
+
+class Car {
+    drive() {
+        console.log("Driving...")
+    }
+}
+
+class Truck {
+    drive() {
+        console.log("Driving a truck...")
+    }
+
+    loadCargo(amount: number) {
+        console.log("Loading Cargo..." + amount)
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+    // 오타의 위험성을 줄여주는 세 번째 타입가드 방법 = instanceOf
+    if(vehicle instanceof Truck) {
+        vehicle.loadCargo(1000)
+    }
+}
+
+useVehicle(v1);
+useVehicle(v2);
